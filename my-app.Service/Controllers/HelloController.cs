5e06cf55328546d9f6c67c900871cf3fae74e46c
galinareshko <galina.reshko@gmail.com>
2024-06-03 12:01:00 +0000

    Init Env ended

--- my-app.Service/Controllers/HelloController.cs
+++ my-app.Service/Controllers/HelloController.cs
@@ -8,14 +8,14 @@ namespace my_app.Service.Controllers
     {
         [HttpGet]
         [Route("hello4")]
-        public IActionResult GetHello()
+        public IActionResult GetHello4()
         {
             return Ok("Hello, World!");
         }
 
         [HttpGet]
         [Route("hello3")]
-        public IActionResult GetHello()
+        public IActionResult GetHello3()
         {
             return Ok("Hello, Maxim!");
         }
